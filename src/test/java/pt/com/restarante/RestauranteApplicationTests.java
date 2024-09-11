32343d35fdc99080f7cd6c800d5a3972797ef921
João José <joaogjose@outlook.com>
2024-09-11 02:25:25 +0100

    config application

---
+++ src/test/java/pt/com/restarante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.restarante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
